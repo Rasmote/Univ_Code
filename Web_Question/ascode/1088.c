229707102b7a37ed5925fa48f18d26fe43c4d309
Rasmote <workrasmote@gmail.com>
2021-05-24 14:50:50 +0900

    5월 24일 프로그래밍 실습 수업

---
+++ Web_Question/ascode/1088.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main()
+{
+
+    while (1)
+    {
+        int m, n, min, max;
+
+        scanf("%d", &m);
+        if (m == 0)
+            break;
+
+        min = 10000;
+        max = -10000;
+
+        for (int i = 0; i < m; i++)
+        {
+            scanf("%d", &n);
+            if (min > n)
+                min = n;
+            if (max < n)
+                max = n;
+        }
+
+        if (max == min)
+        {
+            printf("Same\n");
+        }
+        else if ((max - min) % 3 == 0)
+        {
+            printf("Yes\n");
+        }
+        else
+        {
+            printf("No\n");
+        }
+    }
+}
